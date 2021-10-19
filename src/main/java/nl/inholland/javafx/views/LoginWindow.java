25391722a63ce9840bb56b4c864d72cd1f11f9cf
Elizabeth-E <elizabeth.erickson21@gmail.com>
2021-10-19 22:50:57 +0200

    login view
    login window set up. no logic just the view

--- src/main/java/nl/inholland/javafx/views/LoginWindow.java
+++ src/main/java/nl/inholland/javafx/views/LoginWindow.java
@@ -1,4 +1,44 @@
 package nl.inholland.javafx.views;
 
-public class LoginWindow {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+public class LoginWindow extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setTitle("Fabulous Cinema -- Login");
+        stage.setWidth(300);
+        stage.setHeight(200);
+
+        GridPane grid = new GridPane();
+
+        Label username = new Label("Username");
+        TextField usernameText = new TextField("Username");
+
+        Label password = new Label("Password");
+        PasswordField passwordText = new PasswordField();
+
+        grid.add(username, 1,1);
+        grid.add(usernameText, 2, 1);
+        grid.add(password, 1,2);
+        grid.add(passwordText, 2,2);
+
+        Button login = new Button("Login");
+
+        grid.add(login, 1,3);
+
+        Scene scene = new Scene(grid);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
